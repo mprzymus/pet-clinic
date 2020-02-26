f836e147c63f35c9ea1369565ea7639e5a479f4a
mprzymus <pzrymus@gmail.com>
2020-02-26 20:34:46 +0100

    map based services implemented

--- pet-clinic-data/src/main/java/mprzymus/petclinic/services/VetService.java
+++ pet-clinic-data/src/main/java/mprzymus/petclinic/services/VetService.java
@@ -2,11 +2,6 @@ package mprzymus.petclinic.services;
 
 import mprzymus.petclinic.model.Vet;
 
-import java.util.Set;
-
-public interface VetService {
+public interface VetService extends CrudService<Vet, Long> {
     Vet findByLastName(String lastName);
-    Vet findById(Long id);
-    Vet save(Vet owner);
-    Set<Vet> findAll();
 }
---
+++ pet-clinic-data/src/main/java/mprzymus/petclinic/services/map/VetMapService.java
@@ -0,0 +1,34 @@
+package mprzymus.petclinic.services.map;
+
+import mprzymus.petclinic.model.Vet;
+import mprzymus.petclinic.services.CrudService;
+
+import java.util.Set;
+
+public class VetMapService extends AbstractMapService<Vet, Long> implements CrudService<Vet, Long> {
+
+    @Override
+    public Set<Vet> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        super.deleteById(id);
+    }
+
+    @Override
+    public void delete(Vet object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Vet findById(Long id) {
+        return super.findById(id);
+    }
+
+    @Override
+    public Vet save(Vet object) {
+        return super.save(object.getId(), object);
+    }
+}
