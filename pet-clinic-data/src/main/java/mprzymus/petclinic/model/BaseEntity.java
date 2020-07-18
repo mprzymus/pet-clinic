50d4e509b1ef2f178c4e0908249d77f6405ca56b
mprzymus <przymus@gmail.com>
2020-07-18 19:18:43 +0200

    Updated base entity with JPA annotation

--- pet-clinic-data/src/main/java/mprzymus/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/mprzymus/petclinic/model/BaseEntity.java
@@ -1,8 +1,18 @@
 package mprzymus.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
+@MappedSuperclass
 public class BaseEntity implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
     public Long getId() {
         return id;
     }
@@ -11,5 +21,4 @@ public class BaseEntity implements Serializable {
         this.id = id;
     }
 
-    private Long id;
 }
