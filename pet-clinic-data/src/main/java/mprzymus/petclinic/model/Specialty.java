845465ab4ab38050a9bc89a7f59168163880317a
mprzymus <przymus@gmail.com>
2020-07-21 00:14:53 +0200

    vet convert to entity

--- pet-clinic-data/src/main/java/mprzymus/petclinic/model/Specialty.java
+++ pet-clinic-data/src/main/java/mprzymus/petclinic/model/Specialty.java
@@ -1,6 +1,13 @@
 package mprzymus.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Specialties")
 public class Specialty extends BaseEntity{
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
