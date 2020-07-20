86dc13b14601a53f0b23b2372baf9e1b1d3efa0b
mprzymus <przymus@gmail.com>
2020-07-21 00:08:07 +0200

    user convert to entity

--- pet-clinic-data/src/main/java/mprzymus/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/mprzymus/petclinic/model/PetType.java
@@ -1,6 +1,12 @@
 package mprzymus.petclinic.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "types")
 public class PetType extends BaseEntity{
+
     private String name;
 
     public String getName() {
