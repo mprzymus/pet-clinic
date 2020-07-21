7a1d7ec676133154881e8c35298be617e2c65800
mprzymus <przymus@gmail.com>
2020-07-22 00:32:03 +0200

    Visit into entity turned

--- pet-clinic-data/src/main/java/mprzymus/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/mprzymus/petclinic/model/Vet.java
@@ -7,6 +7,7 @@ import java.util.Set;
 @Entity
 @Table(name = "vets")
 public class Vet extends Person {
+
     @ManyToMany(fetch = FetchType.EAGER)
     @JoinTable(name = "vet_specialties", joinColumns = @JoinColumn(name = "vet_id"),
             inverseJoinColumns = @JoinColumn(name ="speciality_id"))
