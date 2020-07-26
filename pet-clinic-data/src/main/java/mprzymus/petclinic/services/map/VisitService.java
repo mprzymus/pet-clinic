c92744a18c89eb6a2c33011ac64634b3ed8110bc
mprzymus <przymus@gmail.com>
2020-07-26 20:57:06 +0200

    Implemented VisitMapService

---
+++ pet-clinic-data/src/main/java/mprzymus/petclinic/services/map/VisitService.java
@@ -0,0 +1,8 @@
+package mprzymus.petclinic.services.map;
+
+import mprzymus.petclinic.model.Visit;
+import mprzymus.petclinic.services.CrudService;
+
+public interface VisitService extends CrudService<Visit, Long> {
+
+}
