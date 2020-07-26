4f9551a15b0c840a1391c5d5f7cdc9d70dedbdcd
mprzymus <przymus@gmail.com>
2020-07-26 21:02:51 +0200

    VisitSDJpaService implemented

---
+++ pet-clinic-data/src/main/java/mprzymus/petclinic/services/jpa/VisitSDJpaService.java
@@ -0,0 +1,45 @@
+package mprzymus.petclinic.services.jpa;
+
+import mprzymus.petclinic.model.Visit;
+import mprzymus.petclinic.repositories.VisitRepository;
+import mprzymus.petclinic.services.VisitService;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class VisitSDJpaService implements VisitService {
+    private final VisitRepository repository;
+
+    public VisitSDJpaService(VisitRepository repository) {
+        this.repository = repository;
+    }
+
+    @Override
+    public Set<Visit> findAll() {
+        var visits = new HashSet<Visit>();
+        repository.findAll().forEach(visits::add);
+        return visits;
+    }
+
+    @Override
+    public Visit findById(Long id) {
+        return repository.findById(id).orElse(null);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        return repository.save(visit);
+    }
+
+    @Override
+    public void delete(Visit visit) {
+        repository.delete(visit);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        repository.deleteById(id);
+    }
+}
