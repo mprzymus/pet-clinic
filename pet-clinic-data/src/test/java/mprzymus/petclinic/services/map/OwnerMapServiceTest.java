57197740c10212f2f8754bc10cfe643b8115a6f2
mprzymus <przymus@gmail.com>
2020-08-02 17:31:44 +0200

    ownerServiceTest

---
+++ pet-clinic-data/src/test/java/mprzymus/petclinic/services/map/OwnerMapServiceTest.java
@@ -0,0 +1,66 @@
+package mprzymus.petclinic.services.map;
+
+import mprzymus.petclinic.model.Owner;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OwnerMapServiceTest {
+
+    private OwnerMapService ownerMapService;
+    private final Long ownerId = 1L;
+    private final String surname = "Smith";
+
+    @BeforeEach
+    void setUp() {
+        ownerMapService = new OwnerMapService(new PetTypeMapService(), new PetMapService());
+        ownerMapService.save(Owner.builder().id(ownerId).lastName(surname).build());
+    }
+
+    @Test
+    void findAll() {
+        var ownerSet = ownerMapService.findAll();
+        assertEquals(1, ownerSet.size());
+    }
+
+    @Test
+    void deleteById() {
+        ownerMapService.deleteById(ownerId);
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void delete() {
+        ownerMapService.delete(ownerMapService.findById(ownerId));
+        assertEquals(0, ownerMapService.findAll().size());
+    }
+
+    @Test
+    void findById() {
+        var owner = ownerMapService.findById(ownerId);
+        assertEquals(ownerId, owner.getId());
+    }
+
+    @Test
+    void saveExistingId() {
+        Long id = 2L;
+        var owner2 = Owner.builder().id(id).build();
+        var savedOwner = ownerMapService.save(owner2);
+        assertEquals(id, savedOwner.getId());
+    }
+
+    @Test
+    void saveNoId() {
+        var savedOwner = ownerMapService.save(Owner.builder().build());
+        assertNotNull(savedOwner);
+        assertNotNull(savedOwner.getId());
+    }
+
+    @Test
+    void findByLastName() {
+        var smith = ownerMapService.findByLastName(surname);
+        assertNotNull(smith);
+        assertEquals(surname, smith.getLastName());
+    }
+}
