93dc0e0e1e724df11a920c872e222a186e3562f4
mprzymus <przymus@gmail.com>
2020-08-03 21:48:18 +0200

    ownersController tests implemented

---
+++ pet-clinic-web/src/test/java/mprzymus/petclinic/controllers/OwnersControllerTest.java
@@ -0,0 +1,20 @@
+package mprzymus.petclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class OwnersControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void listOwners() {
+    }
+
+    @Test
+    void findOwners() {
+    }
+}
