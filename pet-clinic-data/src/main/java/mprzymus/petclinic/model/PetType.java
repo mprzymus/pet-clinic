2c88abbfeeeb253e1c71affba66ed6f4c06871ba
mprzymus <przymus@gmail.com>
2020-08-31 22:37:28 +0200

    builder refactor

--- pet-clinic-data/src/main/java/mprzymus/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/mprzymus/petclinic/model/PetType.java
@@ -8,12 +8,17 @@ import javax.persistence.Table;
 @Entity
 @Getter
 @Setter
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "types")
 public class PetType extends BaseEntity{
     private String name;
+
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
 
     @Override
     public String toString() {
