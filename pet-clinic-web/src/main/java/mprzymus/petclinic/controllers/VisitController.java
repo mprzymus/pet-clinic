bb958d708435971f583c81a0ed74cfae6ae5f361
mprzymus <przymus@gmail.com>
2020-09-01 16:16:35 +0200

    adding visit

---
+++ pet-clinic-web/src/main/java/mprzymus/petclinic/controllers/VisitController.java
@@ -0,0 +1,55 @@
+package mprzymus.petclinic.controllers;
+
+import lombok.RequiredArgsConstructor;
+import mprzymus.petclinic.model.Pet;
+import mprzymus.petclinic.model.Visit;
+import mprzymus.petclinic.repositories.VisitRepository;
+import mprzymus.petclinic.services.PetService;
+import mprzymus.petclinic.services.VisitService;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.Map;
+
+@Controller
+@RequiredArgsConstructor
+public class VisitController {
+
+    private final VisitService visitService;
+    private final PetService petService;
+
+    @InitBinder
+    public void setAllowedFields(WebDataBinder dataBinder) {
+        dataBinder.setDisallowedFields("id");
+    }
+
+    @ModelAttribute("visit")
+    public Visit loadPetWithVisit(@PathVariable Long petId, Model model) {
+        Pet pet = petService.findById(petId);
+        model.addAttribute("pet", pet);
+        Visit visit = new Visit();
+        pet.getVisits().add(visit);
+        visit.setPet(pet);
+        return visit;
+    }
+
+    @GetMapping("owners/*/pets/{petId}/visits/new")
+    public String initNewVisitForm(@PathVariable Long petId, Model model) {
+        return "pets/createOrUpdateVisitForm";
+    }
+
+    @PostMapping("/owners/{ownerId}/pets/{petId}/visits/new")
+    public String processNewVisitForm(@Valid Visit visit, BindingResult result) {
+        if (result.hasErrors()) {
+            return "pets/createOrUpdateVisitForm";
+        }
+        else {
+            visitService.save(visit);
+            return "redirect:/owners/{ownerId}";
+        }
+    }
+}
---
+++ pet-clinic-web/src/test/java/mprzymus/petclinic/controllers/VisitControllerTest.java
@@ -0,0 +1,18 @@
+package mprzymus.petclinic.controllers;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitControllerTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @Test
+    void loadPetWithVisitTest() {
+
+    }
+}
