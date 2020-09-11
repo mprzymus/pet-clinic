897414ddb0018fedf2aac97b7309ce365769ddbb
mprzymus <przymus@gmail.com>
2020-09-11 20:00:14 +0200

    sorted by last name

--- pet-clinic-web/src/main/java/mprzymus/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/mprzymus/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package mprzymus.petclinic.controllers;
 
+import mprzymus.petclinic.model.Person;
+import mprzymus.petclinic.model.Vet;
 import mprzymus.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 @Controller
 public class VetController {
@@ -16,7 +21,10 @@ public class VetController {
 
     @RequestMapping({"/vets.html", "/vets", "/vets.index", "/vets.index.html"})
     public String listVets(Model model) {
-        model.addAttribute("vets", vetService.findAll());
+        var sortedVets = vetService.findAll().stream()
+                .sorted(Comparator.comparing(Person::getLastName))
+                .collect(Collectors.toList());
+        model.addAttribute("vets", sortedVets);
         return "vets/index";
     }
 }
